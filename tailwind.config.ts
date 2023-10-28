feabc97458b9ed443fbd0e1b70820f7fe1c50030
unknown <yutaagra@gmail.com>
2023-10-28 17:55:01 +0700

    Production: Fixed Configuration tailwind.config.ts in Typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      typography: ({ theme }) => ({
+      typography: ({theme} : { theme: any })=> ({
         blue: {
           css: {
             "--tw-prose-body": theme("colors.blue[800]"),
